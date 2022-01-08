21556b41d4e695649426f47e4e624468575333f6
Ali Dogus TARHAN <you@example.com>
2022-01-08 23:06:32 +0300

    Template Literals

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const express = require('express');
+const ejs=require('ejs');
+const path=require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-
-  res.send(blog);
+  res.render('index');
+  
 });
 
+app.get('/about', (req, res) => {
+  res.render('about');
+  
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+  
+});
+
+
+
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//TEMPLATE ENGINE
+app.set("view engine","ejs");
+
+
+
+
+
+
+//Public klasrümüzü uygulamamıza kaydetmiş olduk.
 const port = 3000;
 
 app.listen(port, () => {});
